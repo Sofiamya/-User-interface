dcbdd193e23fe3132e84b90b80b3f671de15f1d9
Karpova Sofia <monas6991@gmail.com>
2020-09-30 22:43:02 +0300

    # - Задание к первому уроку

---
+++ lesson1/lesson1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  lesson1
+//
+//  Created by Сергей Злобин on 29.09.2020.
+//  Copyright © 2020 karpova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+   
+}
+
